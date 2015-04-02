fca29d2ab2d19dce7d9d359db0a443d7f11915ef
Paul <pt2323@gmail.com>
2015-04-01 22:02:37 -0400

    add crud routes

--- index.js
+++ index.js
@@ -1,28 +1,142 @@
+/**
+
+TODO: Input sanitiazation and validation
+      Add user authentication checking
+      Add flagging routes
+
+*/
+
+/*******************************************************************************
+
+                    SETUP
+
+*******************************************************************************/
+
+var ERROR_CASTING_VOTE = 'An error occured while trying to cast vote.'
+var ERROR_DELETEING_RECORD = 'An error occured while trying to delete your record.'
+var ERROR_SAVING_RECORD = 'An error occured while trying to save your record.'
+var ERROR_RETRIEVING_RECORDS = 'An error occured while trying to get your records.'
+
 var SqlComment = require('sql_comment')
 
-// Todo: validation (all around)
+var getReponseObject = function(){
+    return{
+        success: true,
+        errorMessage: '',
+        errorCode: null,
+        data: null
+    }
+}
 
-module.exports = function(app, settings) {
+var handleDbResponse = function(err, errorMessage, res){
+
+    var responseObject = getReponseObject()
+
+    if( err ){
+        responseObject.success = false
+        responseObject.errorMessage = errorMessage
+        res.json(responseObject)
+    } else {
+        res.json(responseObject)
+    }
+}
+
+/*******************************************************************************
+
+                    MODULE
+
+*******************************************************************************/
+
+module.exports = function(settings){
+
+    var self = this
+    var app = require('express')()
 
     // see sql_comment docs for settings
-    sqlComment = new sqlComment(settings)
+    self.sqlComment = new SqlComment(settings, function(err){
+        if( err ){ throw(err) }
+    })
 
-// sqlComment.add(userId, postId, 0, 'This is a comment', callbackB)
 
-    // parentId should be 0 if comment is top level
-    // 
-    app.post('/post/:userId/:postId/:parentId', function(req, res, next){
-        sqlComment.add(req.params.userId,
-                       req.params.postId,
-                       req.params.parentId,
-                       req.body.comment
-                       function(err){
+/*******************************************************************************
 
-            if( err ){
-                res.send('fail!');
-            } else {
-                res.send('you rock!');
-            }
+                    ROUTES
+
+*******************************************************************************/
+
+
+app.get('/test', function(req, res){
+    res.send('this is only a test')
+})
+
+    app.delete('/comment/:commentId', function(req, res){
+        sqlComment.delete(req.params.commentId, function(req, res){
+            handleDbResponse(err, res, ERROR_DELETEING_RECORD)
         })
     })
-};
+
+    // passes back only non-deleted comments for post
+    app.get('/comments/:postId', function(req, res){
+        sqlComment.getComments(req.params.postId, false, function(err){
+            handleDbResponse(err, ERROR_RETRIEVING_RECORDS, res)
+        })
+    })
+
+    // passes back all comments for post
+    app.get('/comments-all/:postId', function(req, res){
+        sqlComment.getComments(req.params.postId, true, function(err){
+            handleDbResponse(err, ERROR_RETRIEVING_RECORDS, res)
+        })
+    })
+
+    // sqlComment.getFormattedComments(postId, true, function(err, comments){
+    // do not include deleted comments
+    app.get('/comments-formatted/:postId', function(req, res){
+        sqlComment.getFormattedComments(req.params.postId,
+                                        false,
+                                        function(err, comments){
+
+            handleDbResponse(err, ERROR_RETRIEVING_RECORDS, res)
+        })
+    })
+
+    // include deleted comments
+    app.get('/comments-formatted/:postId', function(req, res){
+        sqlComment.getFormattedComments(req.params.postId,
+                                        true,
+                                        function(err, comments){
+
+            handleDbResponse(err, ERROR_RETRIEVING_RECORDS, res)
+        })
+    })
+
+    // sqlComment.add(userId, postId, 0, 'This is a comment', callbackB)
+    // parentId should be 0 if comment is top level
+    app.post('/post/:postId/:parentId', function(req, res, next){
+        self.sqlComment.add(req.body.userId,
+                            req.params.postId,
+                            req.params.parentId,
+                            req.body.comment,
+                            function(err){
+
+            handleDbResponse(err, ERROR_SAVING_RECORD, res)
+        })
+    })
+
+    app.post('/comment/vote/:direction/:commentId', function(req, res){
+        if( req.params.direction === 'up' ){
+            var vote = true
+        } else {
+            var vote = false
+        }
+        sqlComment.vote(req.body.userId,
+                        req.params.commentId,
+                        vote,
+                        function(err){
+
+            handleDbResponse(err, ERROR_CASTING_VOTE, res)
+        })
+    })
+
+    return app
+}
