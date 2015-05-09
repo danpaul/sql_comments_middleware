043a2cade5b5f90772b5db501dfad8e3c28abe69
danpaul <pt2323@gmail.com>
2015-05-09 11:46:42 -0400

    testing and cleaning up

--- README.md
+++ README.md
@@ -2,3 +2,44 @@
 
 ## About
 A light wrapper/middleware for [sql_comments](https://github.com/danpaul/sql_comments)
+
+## Example
+Excerpt from Express app:
+
+```
+app.use(require('cookie-parser')(config.cookieSecret)); 
+app.use(session({
+    secret: 'super secret',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// see http://knexjs.org/ for documentation
+var dbCreds = {
+    client: 'mysql',
+    connection: {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'sql_comment',
+        port:  8889
+    }
+}
+
+// installing knex and mysql is required
+var knex = require('knex')(dbCreds);
+
+// see https://github.com/danpaul/sql_comments/blob/master/settings.js
+var options = {
+    'knex': knex
+};
+
+var sqlCommentsMiddleware = require('sql_comments_middleware')(options);
+
+app.use('/discussion', sqlCommentsMiddleware);
+```
+
+## API/Routes
+See ./index.js
--- index.js
+++ index.js
@@ -8,16 +8,16 @@ TODO: Input sanitiazation and validation
 
 /*******************************************************************************
 
-                    SETUP
+                    SETUP / HELPER FUNCTIONS
 
 *******************************************************************************/
 
-var ERROR_CASTING_VOTE = 'An error occured while trying to cast vote.'
-var ERROR_DELETEING_RECORD = 'An error occured while trying to delete your record.'
-var ERROR_SAVING_RECORD = 'An error occured while trying to save your record.'
-var ERROR_RETRIEVING_RECORDS = 'An error occured while trying to get your records.'
+var ERROR_CASTING_VOTE = 'An error occured while trying to cast vote.';
+var ERROR_DELETEING_RECORD = 'An error occured while trying to delete your record.';
+var ERROR_SAVING_RECORD = 'An error occured while trying to save your record.';
+var ERROR_RETRIEVING_RECORDS = 'An error occured while trying to get your records.';
 
-var SqlComment = require('sql_comment')
+var SqlComment = require('sql_comments');
 
 var getReponseObject = function(){
     return{
@@ -25,19 +25,19 @@ var getReponseObject = function(){
         errorMessage: '',
         errorCode: null,
         data: null
-    }
+    };
 }
 
 var handleDbResponse = function(err, errorMessage, res){
 
-    var responseObject = getReponseObject()
+    var responseObject = getReponseObject();
 
     if( err ){
-        responseObject.success = false
-        responseObject.errorMessage = errorMessage
-        res.json(responseObject)
+        responseObject.success = false;
+        responseObject.errorMessage = errorMessage;
+        res.json(responseObject);
     } else {
-        res.json(responseObject)
+        res.json(responseObject);
     }
 }
 
@@ -49,13 +49,13 @@ var handleDbResponse = function(err, errorMessage, res){
 
 module.exports = function(settings){
 
-    var self = this
-    var app = require('express')()
+    var self = this;
+    var app = require('express')();
 
     // see sql_comment docs for settings
     self.sqlComment = new SqlComment(settings, function(err){
-        if( err ){ throw(err) }
-    })
+        if( err ){ throw(err); }
+    });
 
 
 /*******************************************************************************
@@ -64,41 +64,36 @@ module.exports = function(settings){
 
 *******************************************************************************/
 
-
-app.get('/test', function(req, res){
-    res.send('this is only a test')
-})
-
     app.delete('/comment/:commentId', function(req, res){
         sqlComment.delete(req.params.commentId, function(req, res){
             handleDbResponse(err, res, ERROR_DELETEING_RECORD)
-        })
-    })
+        });
+    });
 
     // passes back only non-deleted comments for post
     app.get('/comments/:postId', function(req, res){
         sqlComment.getComments(req.params.postId, false, function(err){
-            handleDbResponse(err, ERROR_RETRIEVING_RECORDS, res)
-        })
-    })
+            handleDbResponse(err, ERROR_RETRIEVING_RECORDS, res);
+        });
+    });
 
     // passes back all comments for post
     app.get('/comments-all/:postId', function(req, res){
         sqlComment.getComments(req.params.postId, true, function(err){
-            handleDbResponse(err, ERROR_RETRIEVING_RECORDS, res)
-        })
-    })
+            handleDbResponse(err, ERROR_RETRIEVING_RECORDS, res);
+        });
+    });
 
     // sqlComment.getFormattedComments(postId, true, function(err, comments){
     // do not include deleted comments
-    app.get('/comments-formatted/:postId', function(req, res){
+    app.get('/comments-formatted-all/:postId', function(req, res){
         sqlComment.getFormattedComments(req.params.postId,
                                         false,
                                         function(err, comments){
 
-            handleDbResponse(err, ERROR_RETRIEVING_RECORDS, res)
-        })
-    })
+            handleDbResponse(err, ERROR_RETRIEVING_RECORDS, res);
+        });
+    });
 
     // include deleted comments
     app.get('/comments-formatted/:postId', function(req, res){
@@ -106,9 +101,9 @@ app.get('/test', function(req, res){
                                         true,
                                         function(err, comments){
 
-            handleDbResponse(err, ERROR_RETRIEVING_RECORDS, res)
-        })
-    })
+            handleDbResponse(err, ERROR_RETRIEVING_RECORDS, res);
+        });
+    });
 
     // sqlComment.add(userId, postId, 0, 'This is a comment', callbackB)
     // parentId should be 0 if comment is top level
@@ -119,24 +114,24 @@ app.get('/test', function(req, res){
                             req.body.comment,
                             function(err){
 
-            handleDbResponse(err, ERROR_SAVING_RECORD, res)
-        })
-    })
+            handleDbResponse(err, ERROR_SAVING_RECORD, res);
+        });
+    });
 
     app.post('/comment/vote/:direction/:commentId', function(req, res){
         if( req.params.direction === 'up' ){
-            var vote = true
+            var vote = true;
         } else {
-            var vote = false
+            var vote = false;
         }
         sqlComment.vote(req.body.userId,
                         req.params.commentId,
                         vote,
                         function(err){
 
-            handleDbResponse(err, ERROR_CASTING_VOTE, res)
-        })
-    })
+            handleDbResponse(err, ERROR_CASTING_VOTE, res);
+        });
+    });
 
-    return app
+    return app;
 }
